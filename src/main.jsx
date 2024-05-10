7b47ed845730366496edaec1f6749c616a5900bc
Coquimbo771 <axelcoaqui@gmail.com>
2024-05-10 10:04:10 -0400

    tarjeta terminada

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react';
+import './index.css';
+
+function main() {
+  return (
+    <div className="card">
+      <img src="imagen-superior.jpg" className="card-img-top" alt="Imagen superior" />
+      <div className="card-body">
+        <h5 className="card-title">Título</h5>
+        <p className="card-text">Descripción corta del video o contenido.</p>
+        <a href="#" className="btn btn-primary">Play</a>
+      </div>
+      <img src="imagen-inferior.jpg" className="card-img-bottom" alt="Imagen inferior" />
+    </div>
+  );
+}
+
+export default main;
+
